c6dc79a99ac26986129f7aa0b24298e5fa22c4ab
Pabl0t0rr <Pablotorr04@gmail.com>
2026-02-16 11:43:11 +0100

    Modificacion archivo

--- Teoria/Semana_3/Lunes-Viernes/src/App.tsx
+++ Teoria/Semana_3/Lunes-Viernes/src/App.tsx
@@ -1,11 +1,16 @@
 import { useState } from 'react';
 import './App.css';
 import type { CharacterT} from './types';
-import { Character }from './components/Character';
+import { CharacterById, CharacterByName }from './components/Character';
 
 const App = () => {
-  const [character, setCharacter] = useState<CharacterT | null>(null);
   const [id, setId] = useState<number>(1);
+  const [finalId, setFinalId] = useState<number | undefined>(undefined);
+
+  const [nombre, setNombre] = useState<string>("");
+  const [palabraFinal, setPalabraFinal] = useState<string>("");
+
+  const [characters,setCharacters] = useState<CharacterT[]>([]);
 
   return (
     <>
@@ -13,8 +18,21 @@ const App = () => {
       <p>El id escrito es: {id}</p>
       <input value = {id}
       onChange={ (e) => {setId(Number(e.target.value))}}></input> {/* para meter textos */}
+      <button onClick={() => setFinalId(id)}>Buscar por Id</button>
+      <CharacterById id = {finalId} />
     </div>
-    <Character id={id}/>
+
+    <div>
+      <p>El nombre escrito es: {palabraFinal}</p>
+      <input value = {palabraFinal}
+      onChange={ (e) => {setPalabraFinal(e.target.value)}}></input>
+      <button onClick={() => setPalabraFinal(palabraFinal)}>Buscar por Nombre</button>
+
+      {characters.map((e) => (
+        <CharacterByName key={e.id} character={e} />
+      ))}
+    </div>
+     
    </>
   )
 }
--- Teoria/Semana_3/Lunes-Viernes/src/components/Character/index.tsx
+++ Teoria/Semana_3/Lunes-Viernes/src/components/Character/index.tsx
@@ -3,7 +3,7 @@ import { api } from "../../api/api";
 import type { CharacterT } from "../../types";
 import "./style.css";
 
-export const Character = (params : {id ?: number, character ?: CharacterT}) => {
+export const CharacterById = (params : {id ?: number, character ?: CharacterT}) => {
 
     const id = params.id;
 
@@ -11,7 +11,7 @@ export const Character = (params : {id ?: number, character ?: CharacterT}) => {
 
   useEffect(() => {
     !params.character && id && api.get(`/character/${id}`).then ((e) => setPersonaje(e.data));
-  }, [id]) //Si se le pasa una variable de estado en el [] se volvera a ejecutar cada vez que se actualiza el estado
+  }, [id]) //Si se le pasa una variable de estado en el [] se volvera a ejecutar cada vez que se actualiza el [id]
 
     return (
         <>
@@ -26,4 +26,31 @@ export const Character = (params : {id ?: number, character ?: CharacterT}) => {
         </div> : <h1>LOADING...</h1>}
         </>
     )
-} 
+}
+
+
+export const CharacterByName = (params : {name ?: string, character ?: CharacterT}) => {
+
+    const name = params.name;
+
+    const [personaje, setPersonaje] = useState<CharacterT | null>(null);
+
+    useEffect(() => {
+         !params.character && name && api.get(`/character/${name ? "?name=" + name : ""}`)
+         .then ((e) => setPersonaje(e.data.results));
+    }, [name])
+
+    return (
+        <>
+        {personaje ? <div className = "mainContainer">
+            {personaje?.image && <img src={personaje?.image}/>}
+            <div className="characterContainer">
+                <h2>{personaje?.name}</h2>
+                <p>Status: {personaje?.status}</p>
+                <p>Species: {personaje?.species}</p>
+                <p>Type: {personaje?.type}</p>
+            </div>
+        </div> : <h1>LOADING...</h1>}
+        </>
+    )
+}
