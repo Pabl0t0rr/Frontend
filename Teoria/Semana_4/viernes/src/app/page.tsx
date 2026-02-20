8689690d383612c88f4af8ba5d512fa00dcb328c
Pabl0t0rr <Pablotorr04@gmail.com>
2026-02-20 16:20:27 +0100

    Contenido clase viernes

--- Teoria/Semana_4/viernes/src/app/page.tsx
+++ Teoria/Semana_4/viernes/src/app/page.tsx
@@ -1,10 +1,25 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'; //Necesario para usar hooks en componentes de cliente
+
+import Link from "next/link";
+import { useRouter } from "next/navigation"; //Importar el nuevo
 
 const Home = () => {
+
+  const router = useRouter(); //Si empieza por "use" son hooks
+
   return (
-    <div>Hola</div>
+    <div>
+      <h1>Pagina ruta principal</h1>
+      <Link href="/patata">Te lleva a patata</Link>
+      <br />
+      <button onClick={() => 
+        router.push("/patata")}
+        >Te lleva a patata con un boton</button>
+      <button onClick={() => 
+        router.replace("/patata")}
+        >Te lleva a patata con un boton pero sin historial</button>
+    </div>
   );
 }
 
-export default Home();
+export default Home;
