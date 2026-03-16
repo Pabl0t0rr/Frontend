a0e6d163bfcbf09ef2699b50de60c7c41a87254b
Pabl0t0rr <Pablotorr04@gmail.com>
2026-03-16 09:13:15 +0100

    tabulados

--- Teoria/Semana_5/viernes/src/app/drink/[id]/page.tsx
+++ Teoria/Semana_5/viernes/src/app/drink/[id]/page.tsx
@@ -1,63 +1,58 @@
-'use client'
+"use client";
 
-import { getCocktailById} from "@/lib/api/cocktail";
+import { getCocktailById } from "@/lib/api/cocktail";
 import { Drink } from "@/types";
 import { AxiosError } from "axios";
 import { useParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import "./drink.css"
+import "./drink.css";
 
 const getId = () => {
+  const { id } = useParams();
 
-    const {id} = useParams();
+  const router = useRouter();
 
-    const router = useRouter();
+  const [cocktail, setCocktail] = useState<Drink | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
 
-    const [cocktail, setCocktail] = useState<Drink | null>(null);
-    const [loading, setLoading] = useState<boolean>(true);
-    const [error, setError] = useState<string | null> (null);
+  useEffect(() => {
+    getCocktailById(Number(id))
+      .then((res: any) => {
+        setCocktail(res.drinks[0]);
+      })
+      .catch((err: AxiosError) => {
+        setError(err.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [id]);
 
-    
+  return (
+    <div className="mainContainer">
+      {!loading && !error && cocktail && (
+        <>
+          <img src={cocktail?.strDrinkThumb}></img>
 
-    useEffect(() => {
+          <div className="drinkDataContainer">
+            <p>Nombre: {cocktail?.strDrink}</p>
+            <p>Nombre: {cocktail?.strDrink}</p>
+            <p>Nombre: {cocktail?.strDrink}</p>
+            <p>Nombre: {cocktail?.strDrink}</p>
+            <p>Nombre: {cocktail?.strDrink}</p>
+            <p>Nombre: {cocktail?.strDrink}</p>
+            <p>Nombre: {cocktail?.strDrink}</p>
 
-        getCocktailById(Number(id)).then ( (res : any) => {
-            setCocktail(res.drinks[0]);
-        }).catch((err: AxiosError) => {
-            setError(err.message);
-        }).finally(() => {
-            setLoading(false);
-        })
-    },  [id])
+            <button onClick={() => router.back()}>Volver</button>
+          </div>
+        </>
+      )}
 
-
-    return (
-         <div className="mainContainer">
-            {!loading && !error && cocktail && (
-                <>
-                    <img src={cocktail?.strDrinkThumb}></img>
-
-                    <div className="drinkDataContainer">
-                    
-                    <p>Nombre: {cocktail?.strDrink}</p>
-                    <p>Nombre: {cocktail?.strDrink}</p>
-                    <p>Nombre: {cocktail?.strDrink}</p>
-                    <p>Nombre: {cocktail?.strDrink}</p>
-                    <p>Nombre: {cocktail?.strDrink}</p>
-                    <p>Nombre: {cocktail?.strDrink}</p>
-                    <p>Nombre: {cocktail?.strDrink}</p>
-
-                     <button onClick={()=> router.back()}>Volver</button>
-                    </div>
-                </>
-            )}   
-                    
-            {!cocktail && loading && <p>Loading...</p>}
-            {error && <p>Error: {error}</p>}
-        </div>
-        
-
-    )
+      {!cocktail && loading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+    </div>
+  );
 };
 
 export default getId;
--- Teoria/Semana_5/viernes/src/app/page.tsx
+++ Teoria/Semana_5/viernes/src/app/page.tsx
@@ -1,14 +1,13 @@
-'use client'
+"use client";
 
 import { getCocktailsByName } from "@/lib/api/cocktail";
 import { Drink } from "@/types";
 import { AxiosError } from "axios";
 import { useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
-import "./drink.css"
+import "./drink.css";
 
 const Home = () => {
-
   //Para el buscador
   const [name, setName] = useState<string>("");
   const [finalName, setFinalName] = useState<string>("");
@@ -21,62 +20,68 @@ const Home = () => {
   const [loading, setLoading] = useState<Boolean>(false);
 
   useEffect(() => {
-
-    if(!finalName) return; //Para evitar hacer una llamada a la API
+    if (!finalName) return; //Para evitar hacer una llamada a la API
     //Inicializar estados
     setLoading(true);
     setError(null);
 
-    getCocktailsByName(finalName).then((d)=> {
-      setDrinks(d.drinks);
-    }).catch((err : AxiosError) => {
-      setError(err.message);
-    }).finally(() => {
-      setLoading(false);
-    })
-  
+    getCocktailsByName(finalName)
+      .then((d) => {
+        setDrinks(d.drinks);
+      })
+      .catch((err: AxiosError) => {
+        setError(err.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   }, [finalName]);
-
 
   return (
     <div>
       <h1>Buscador de cócteles</h1>
-        <div>
-          <input onChange={(n) => setName(n.target.value)}></input>
-          <button onClick={() => {
-            setFinalName(name)
-          }}>Buscar coctel</button>
+      <div>
+        <input onChange={(n) => setName(n.target.value)}></input>
+        <button
+          onClick={() => {
+            setFinalName(name);
+          }}
+        >
+          Buscar coctel
+        </button>
+      </div>
+      {loading && <h2> Loading...</h2>}
+      {error && <h2> Error: {error}</h2>}
 
-        </div>
-     {loading && <h2> Loading...</h2>}
-     {error && <h2> Error: {error}</h2>}
-
-     <div>
-      {!loading && !error && drinks.length > 0 && 
-      drinks.map((drinks) => {
-        return (
-          <>
-            <div className="mainContainer" key = {drinks.idDrink}>
-              <button onClick={() => {
-                router.push("/drink/"+ drinks.idDrink)
-              }}>
-                {drinks?.strDrinkThumb && <img src = {drinks.strDrinkThumb}></img>}
-                <div className="drinkDataContainer">
-                  <p>Name: {drinks?.strDrink}</p>
-                  <p>Category: {drinks?.strCategory}</p>
-                  <p>Glass: {drinks?.strGlass}</p>
-
+      <div>
+        {!loading &&
+          !error &&
+          drinks.length > 0 &&
+          drinks.map((drinks) => {
+            return (
+              <>
+                <div className="mainContainer" key={drinks.idDrink}>
+                  <button
+                    onClick={() => {
+                      router.push("/drink/" + drinks.idDrink);
+                    }}
+                  >
+                    {drinks?.strDrinkThumb && (
+                      <img src={drinks.strDrinkThumb}></img>
+                    )}
+                    <div className="drinkDataContainer">
+                      <p>Name: {drinks?.strDrink}</p>
+                      <p>Category: {drinks?.strCategory}</p>
+                      <p>Glass: {drinks?.strGlass}</p>
+                    </div>
+                  </button>
                 </div>
-              </button>
-            </div>
-          </>
-        )
-      })}
-     </div>
-
+              </>
+            );
+          })}
+      </div>
     </div>
-  )
-
-}
+  );
+};
 
 export default Home;
