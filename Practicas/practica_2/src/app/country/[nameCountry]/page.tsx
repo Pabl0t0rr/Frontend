e17d606649f3f6dc048bb53c9cf67334db38790e
Pabl0t0rr <Pablotorr04@gmail.com>
2026-04-14 17:43:57 +0200

    Añadidas las practicas 3 y 2 usando react

---
+++ Practicas/practica_2/src/app/country/[nameCountry]/page.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import { useParams, useRouter } from 'next/navigation'
+import { Country } from '@/types'
+import { useEffect, useState } from 'react'
+import { getCountryByFullName } from '@/lib/api/country'
+import { AxiosError } from 'axios'
+
+import './page.css'
+
+const getCountryInfo = () => {
+   const { nameCountry } = useParams()
+
+   const router = useRouter()
+
+   const [pais, setPais] = useState<Country | null>(null)
+   const [loading, setLoading] = useState<boolean>(false)
+   const [error, setError] = useState<string | null>(null)
+
+   useEffect(() => {
+      if (!nameCountry) return
+
+      setLoading(true)
+      setError(null)
+
+      getCountryByFullName(nameCountry as string)
+         .then((c) => {
+            setPais(c)
+         })
+         .catch((e: AxiosError) => {
+            setError(e.message)
+         })
+         .finally(() => {
+            setLoading(false)
+         })
+   }, [nameCountry])
+
+   return (
+      <>
+         <div className="mainContainer">
+            {!loading && !error && pais && (
+               <div className="country-info">
+                  <p>Nombre oficial: {pais?.name.official}</p>
+                  <p>Bandera: {pais?.flag}</p>
+                  <p>Capital: {pais?.capital}</p>
+                  <p>Region: {pais?.region}</p>
+                  <p>Poblacion: {pais?.population}</p>
+                  <p>Idiomas: {Object.values(pais?.languages || {}).join(', ')}</p>
+                  <button onClick={() => router.back()}>Volver</button>
+               </div>
+            )}
+
+            {!pais && loading && <p>Loading...</p>}
+            {error && <p>Error: {error}</p>}
+         </div>
+      </>
+   )
+}
+
+export default getCountryInfo
